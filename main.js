a643d5d912a5e51784ba1115a28c0e11d2d87949
Akinwole Adam <100495971+AkinwoleAdam@users.noreply.github.com>
2022-07-16 11:54:27 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+
+/*------NavLink hide and show menu------*/
+
+var navLinks=document.getElementById("navLinks");
+
+function showMenu(){
+
+navLinks.style.right="0"
+}
+
+function hideMenu(){
+
+navLinks.style.right="-200px";
+}
